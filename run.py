7a6f1bc30f05ceef6e1e672665b2bcb57fb7eada
Bovine Hero <garygrant79@yahoo.co.uk>
2022-12-11 13:44:30 +0000

    Init Commit with

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import shodan
+
+#change this to ENVs or getpass.getpass
+SHODAN_API_KEY = input('Enter your Shodan API Key: ')
+
+api = shodan.Shodan(SHODAN_API_KEY)
+info = api.host('8.8.8.8')
+
+
+print(info)
