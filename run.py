4762a9803af7c1aa823c2e7e5cfba57338e6741b
Bovine Hero <garygrant79@yahoo.co.uk>
2022-12-17 12:38:00 +0000

    adding gdrive scope

--- run.py
+++ run.py
@@ -1,29 +1,41 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 import shodan
+import gspread
+from google.oauth2.service_account import Credentials
+
+""" Google Drive Scope for project """
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
 
 def poc():
     """ just a poc function """
-    #change this to ENVs or getpass.getpass
-    SHODAN_API_KEY = input('Enter your Shodan API Key: ')
-    api = shodan.Shodan(SHODAN_API_KEY)
-    info = api.host('8.8.8.8')
-    print(info)
+
+    pass
+    # change this to ENVs or getpass.getpass
+    # SHODAN_API_KEY = input('Enter your Shodan API Key: ')
+    # api = shodan.Shodan(SHODAN_API_KEY)
+    # info = api.host('8.8.8.8')
+    # print(info)
 
 
 def main():
     """ main finction to handle the runtime """
     poc()
 
-    # input IP 
-    # IP validator 
-    # Scan selector 
-    # Scan validator 
-    # Execute Scan 
-    # Scan Incrementor 
-    # Scan Logger 
-    # Display Results 
+    # input IP
+    # IP validator
+    # Scan selector
+    # Scan validator
+    # Execute Scan
+    # Scan Incrementor
+    # Scan Logger
+    # Display Results
 
 
 if __name__ == "__main__":
