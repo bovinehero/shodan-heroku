2f4c71687b8e488418ac60e7fbae4746a5e2403e
Bovine Hero <garygrant79@yahoo.co.uk>
2022-12-17 13:34:21 +0000

    error handling for gspread

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+""" Main Script Run"""
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
@@ -6,7 +8,8 @@ import shodan
 import gspread
 from google.oauth2.service_account import Credentials
 
-""" Google Drive Scope for project """
+# Google Drive Scope for project
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -24,15 +27,21 @@ SHEET = GSPREAD_CLIENT.open(title=SHEET_TITLE)
 
 def fetch_gspread_data(sheet_title="sales"):
     """ fetch all data from worksheet """
-    test_sheet = SHEET.worksheet(title=sheet_title)
-    data = test_sheet.get_all_values()
-    print(data)
+    data = []
+    try:
+        test_sheet = SHEET.worksheet(title=sheet_title)
+        data = test_sheet.get_all_values()
+    except gspread.exceptions.WorksheetNotFound as err:
+        print(f'[!] Unable to find worksheet {err}.')
+    else:
+        print(f'[+] Data Retrieved from {sheet_title}')
+    finally:
+        return data
 
 
 def poc():
     """ just a poc function """
     fetch_gspread_data()
-    pass
     # change this to ENVs or getpass.getpass
     # SHODAN_API_KEY = input('Enter your Shodan API Key: ')
     # api = shodan.Shodan(SHODAN_API_KEY)
